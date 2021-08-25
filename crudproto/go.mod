91324248343b66187e83627eceb465852c6372c7
Vito Di Villa <vitodivilla@gmail.com>
2021-08-25 12:19:02 +0200

    Restructuring

--- crudproto/go.mod
+++ crudproto/go.mod
@@ -1,4 +1,4 @@
-module github.com/divilla/eop09/crudproto
+module github.com/divilla/eop09/crudproto/v0.0.1
 
 go 1.16
 
--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/divilla/eop09/crudproto"
 	"github.com/divilla/eop09/server/config"
 	"github.com/divilla/eop09/server/internal/probe"
 	"github.com/divilla/eop09/server/pkg/cmiddleware"
@@ -16,7 +17,6 @@ import (
 	"os"
 	"os/signal"
 	"time"
-	pb "github.com/divilla/eop09/crudproto"
 )
 
 func init() {
@@ -61,7 +61,7 @@ func main() {
 			log.Fatalf("failed to listen: %v", err)
 		}
 		s := grpc.NewServer()
-		pb.RegisterGreeterServer(s, &server{})
+		crudproto(s, &server{})
 		log.Printf("server listening at %v", lis.Addr())
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("failed to serve: %v", err)
